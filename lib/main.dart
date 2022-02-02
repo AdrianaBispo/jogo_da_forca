519356b404c809666f244372ff6ffcdbb492d5e5
AdrianaBispo <adriana.bispo283@gmail.com>
2022-02-02 16:48:02 -0300

    removido import initial.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
-//import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
-//import 'package:flutter/painting.dart';
-import 'package:jogo_da_forca/pages/inicial.dart';
+import 'my_app.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
-  @override
-
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Home Page',
-      home: HomePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
 
