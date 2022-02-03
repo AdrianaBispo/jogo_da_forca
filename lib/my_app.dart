381d224ec0e8fc766904f7fd8abd1122cfeb7d25
AdrianaBispo <adriana.bispo283@gmail.com>
2022-02-03 16:10:42 -0300

    adcionado as rotas a class MyApp

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:jogo_da_forca/pages/inicial.dart';
+
+//screens
+import 'package:jogo_da_forca/pages/game.dart';
+import 'package:jogo_da_forca/pages/home_page.dart';
+
+//routes
+import 'utils/app_routes.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({ Key? key }) : super(key: key);
   @override
 
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       title: 'Home Page',
-      home: HomePage(),
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: '',
+      ),//theme
+      routes: {
+        AppRoutes.HOME: (ctx) => const HomePage(),
+        AppRoutes.GAME: (ctx) => const SecondPage(),
+      }
     );
   }
 }
