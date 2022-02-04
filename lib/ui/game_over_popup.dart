bbffe2c9b231ec83418c7b0c90a8208f2c22a444
AdrianaBispo <adriana.bispo283@gmail.com>
2022-02-04 14:03:11 -0300

    valor do parametro foi mudado

--- lib/ui/game_over_popup.dart
+++ lib/ui/game_over_popup.dart
@@ -9,7 +9,7 @@ void gameOver(String word, context) {
       builder: (BuildContext context) {
         return popUpModel(
           frase: 'Game Over',
-          word: word,
+          word: 'A palavra era $word',
           color: const Color(0xffd24977),
           pressed: Navigator.pushReplacementNamed(
             context,
