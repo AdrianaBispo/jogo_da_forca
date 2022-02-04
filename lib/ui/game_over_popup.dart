c1edc30f54d228cf2b73c9b8e8f82da4ec8d7597
AdrianaBispo <adriana.bispo283@gmail.com>
2022-02-04 13:24:55 -0300

    game over diminuição do codigo

--- lib/ui/game_over_popup.dart
+++ lib/ui/game_over_popup.dart
@@ -1,49 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:jogo_da_forca/ui/popup_model.dart';
 import 'package:jogo_da_forca/utils/app_routes.dart';
 
 void gameOver(String word, context) {
-    showDialog(
+  showDialog(
       context: context,
       barrierDismissible: false,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text(
-            'Game Over',
-            style: TextStyle(
-              fontWeight: FontWeight.normal,
-              fontSize: 20.0,
-              color: Color(0xff2b73b9),
-            ),
+        return popUpModel(
+          frase: 'Game Over',
+          word: word,
+          color: const Color(0xffd24977),
+          pressed: Navigator.pushReplacementNamed(
+            context,
+            AppRoutes.HOME,
           ),
-          content: Text(
-            'A palavra era $word',
-            style: const TextStyle(
-              fontWeight: FontWeight.normal,
-              fontSize: 17.0,
-              color: Color(0xffd24977),
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: const Color(0xffd24977),
-                ),
-                child: const Text(
-                  'closer',
-                  style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    fontSize: 14.0,
-                    color: Color(0XFFFFFFFF),
-                  ),
-                ),
-                onPressed: () {
-                  Navigator.pushReplacementNamed(
-                    context,
-                    AppRoutes.HOME,
-                  );
-                }),
-          ],
         );
-      },
-    );
-  }
+      });
+}
