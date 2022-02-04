e2c513d9066a5afc94d51b431c88c3551a77ee64
AdrianaBispo <adriana.bispo283@gmail.com>
2022-02-04 15:49:16 -0300

    algumas variaveis constantes

---
+++ lib/constants.dart
@@ -0,0 +1,44 @@
+const List<String> alfabeto = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'x',
+    'w',
+    'y',
+    'z'
+  ];
+
+
+  const List<String> wordData =  [
+    'bicicleta',
+    'sino',
+    'passeio',
+    'escritor',
+    'bolo',
+    'balão',
+    'pé',
+    'chuveiro',
+    'vilão',
+    'porco',
+    'inglês',
+    'coração'
+  ];
