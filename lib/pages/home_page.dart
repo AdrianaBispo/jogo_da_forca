147d61ec70e50245bc02188c5b75ae5eb76f915e
AdrianaBispo <adriana.bispo283@gmail.com>
2022-02-07 14:58:25 -0300

    funcoes e variaveis para chamar o banner

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,13 +6,61 @@ import 'package:jogo_da_forca/utils/app_routes.dart';
 //ads Mobile
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-class HomePage extends StatefulWidget {
+import '../ad_helper.dart';
 
+class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  late BannerAd _bannerAd;
+  bool _isBannerAdReady = false;
+  InterstitialAd? _interstitialAd;
+  bool _isInterstitialAdReady = false;
+  RewardedAd? _rewardedAd;
+  bool _isRewardedAdReady = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _bannerAd = BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      request: const AdRequest(),
+      size: AdSize.banner,
+      listener: BannerAdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isBannerAdReady = true;
+          });
+        },
+        onAdFailedToLoad: (ad, err) {
+          print('Failed to load a banner ad: ${err.message}');
+          _isBannerAdReady = false;
+          ad.dispose();
+        },
+      ),
+    );
+
+    _bannerAd.load();
+  }
+
+    @override
+  void dispose() {
+    // COMPLETE: Dispose a BannerAd object
+    _bannerAd.dispose();
+
+    // COMPLETE: Dispose an InterstitialAd object
+    _interstitialAd?.dispose();
+
+    // COMPLETE: Dispose a RewardedAd object
+    _rewardedAd?.dispose();
+
+    //QuizManager.instance.listener = null;
+
+    super.dispose();
+  }
+
   Widget _buildImagem(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
     MediaQuery.of(context).padding.top;
@@ -67,17 +115,25 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: const Color(0xFFF8F9FD),
       body: FutureBuilder<void>(
-        future: _initGoogleMobileAds(),
-        builder: (context, snapshot) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Center(child: _buildImagem(context)),
-              Center(child: _buildButton())
-            ],
-          );
-        }
-      ),
+          future: _initGoogleMobileAds(),
+          builder: (context, snapshot) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(child: _buildImagem(context)),
+                Center(child: _buildButton()),
+                if (_isBannerAdReady)
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: SizedBox(
+                      width: _bannerAd.size.width.toDouble(),
+                      height: _bannerAd.size.height.toDouble(),
+                      child: AdWidget(ad: _bannerAd),
+                    ),
+                  ),
+              ],
+            );
+          }),
     );
   }
 
