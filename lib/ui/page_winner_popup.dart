e7d64aafebd8e6c0af63a24f869919a574f53517
AdrianaBispo <adriana.bispo283@gmail.com>
2022-02-21 20:06:31 -0300

    documento formatado

--- lib/ui/page_winner_popup.dart
+++ lib/ui/page_winner_popup.dart
@@ -4,15 +4,16 @@ import 'package:jogo_da_forca/utils/app_routes.dart';
 
 gameWinner(String word, context) {
   return showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return popUpModel(
-          frase: 'Parabéns, ',
-          word: 'Você acertou a palavra',
-          color: const Color(0xff49d2a4),
-          context: context,
-          route: Navigator.pushReplacementNamed(context, AppRoutes.HOME),
-        );
-      });
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return popUpModel(
+        frase: 'Parabéns, ',
+        word: 'Você acertou a palavra',
+        color: const Color(0xff49d2a4),
+        context: context,
+        route: Navigator.pushReplacementNamed(context, AppRoutes.HOME),
+      );
+    },
+  );
 }
