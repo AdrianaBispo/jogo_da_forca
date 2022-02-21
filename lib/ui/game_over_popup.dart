e733e64c422b410634fe697301f4d5c6ac67c72a
AdrianaBispo <adriana.bispo283@gmail.com>
2022-02-21 20:21:36 -0300

    arquivo formatado

--- lib/ui/game_over_popup.dart
+++ lib/ui/game_over_popup.dart
@@ -4,15 +4,16 @@ import 'package:jogo_da_forca/utils/app_routes.dart';
 
 gameOver(String word, dynamic context) {
   return showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return popUpModel(
-          frase: 'Game Over',
-          word: 'A palavra era $word',
-          color: const Color(0xffd24977),
-          context: context,
-          route: AppRoutes.HOME,
-        );
-      });
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return popUpModel(
+        frase: 'Game Over',
+        word: 'A palavra era $word',
+        color: const Color(0xffd24977),
+        context: context,
+        route: AppRoutes.HOME,
+      );
+    },
+  );
 }
